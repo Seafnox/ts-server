ca874b29a86a1cdeb4b1983ec716d45d6dab3bc1
k.varpakhovich <kirill9990@gmail.com>
2019-05-06 14:57:25 +0300

    pretty CORS

--- middlewars/cors.ts
+++ middlewars/cors.ts
@@ -6,7 +6,9 @@ export class Cors implements IMiddleware {
     // tslint:disable-next-line:no-any
     public use(@Request() req: any, @Response() res: any): void {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.header('Access-Control-Allow-Methods', req.header('Access-Control-Request-Method'));
+        if (req.header('Access-Control-Request-Method')) {
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            res.header('Access-Control-Allow-Methods', req.header('Access-Control-Request-Method'));
+        }
     }
 }
