c32643627609565e9bbe2f6bef3c5a5910000300
Kirill Warp <kirill9990@gmail.com>
2021-10-24 01:41:59 +0300

    add generate cert script

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,15 +1,23 @@
 import { $log } from '@tsed/common';
 import { PlatformExpress } from '@tsed/platform-express';
+import { readFileSync } from 'fs';
+import { ServerOptions } from 'https';
+import * as path from 'path';
 import { ImageHelper } from './helper/image/image.helper';
 import { Server } from './server/server';
 
 async function bootstrap(): Promise<void> {
     try {
         $log.debug('Start server...');
+        const certPath = path.join(__dirname, "server", "certificates");
         const platform = await PlatformExpress.bootstrap(Server, {
             rootDir: __dirname,
-            httpPort: 'localhost:8080',
+            // httpPort: 'localhost:8080',
             httpsPort: 'localhost:8000',
+            httpsOptions: {
+                key: readFileSync(path.join(certPath, "clientKey.pem")),
+                cert: readFileSync(path.join(certPath, "certificate.pem")),
+            } as ServerOptions,
             uploadDir: `./uploads`,
             acceptMimes: ['application/json'],
             statics: {
---
+++ server/generate-cert.ts
@@ -0,0 +1,28 @@
+import { writeFileSync, mkdirSync, existsSync } from 'fs';
+import * as path from 'path';
+import { createCertificate, config } from 'pem';
+
+config({
+    pathOpenSSL: "C:\\Program Files\\Git\\usr\\bin\\openssl.exe"
+});
+
+createCertificate({ selfSigned: true }, function (err, keys) {
+    if (err) {
+        throw err
+    }
+
+    const { csr, clientKey, serviceKey, certificate } = keys;
+    const certPath = path.join(__filename, "../certificates");
+
+    console.log('GENERATED');
+    console.log(__filename);
+    console.log(certPath);
+
+    if (!existsSync(certPath)) {
+        mkdirSync(certPath);
+    }
+    writeFileSync(path.join(certPath, 'csr.pem'), csr);
+    writeFileSync(path.join(certPath, 'clientKey.pem'), clientKey);
+    writeFileSync(path.join(certPath, 'serviceKey.pem'), serviceKey);
+    writeFileSync(path.join(certPath, 'certificate.pem'), certificate);
+});
